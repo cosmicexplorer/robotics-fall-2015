96673b854c7d190efa6e7a63dffe34d045ffd0bf
Richard <richard.j.hendrick@vanderbilt.edu>
2015-12-03 01:36:00 -0600

    updated strategy to use logtr, figuring out dimension issues

--- test.py
+++ test.py
@@ -53,152 +53,76 @@ def send_to_joint_vals(q):
     print("In home pose")
     return qc
 
-def getD(cur, final, scale):
-    lin = numpy.subtract(final[0:3,0], cur[0:3,0])
-    ang = numpy.subtract(final[3:,0], cur[3:,0])
-    lin_d = numpy.linalg.norm(lin)
-    ang_d = numpy.linalg.norm(ang)
-    return numpy.sqrt((lin_d * scale[0]) ** 2 + (ang_d * scale[1]) ** 2)
+def runResolvedRates(rkin, publish_fun, final, speed, freq, tolerance, alpha, qMax, qMin):
+    util.resolvedRates(
+        alpha, tolerance,
+        lambda: rkin.jacobian(),
+        rospy.Rate(freq),
+        lambda: numpy.matrix([rospy.wait_for_message("/robot/joint_states", 
+                                       JointState).position[9:16]]).T,
+        lambda: getT(rkin),
+        getPosOriVector,
+        publish_fun,
+        final,
+        speed,
+        1./freq)
 
-def runResolvedRates(rkin, final, speed, freq, scale, tolerance, qMax, qMin,
-                     angular_tol):
-    # stupid setup stuff
-    pub_joint_cmd=rospy.Publisher(
-        '/robot/limb/right/joint_command',JointCommand)
-    command_msg = JointCommand()
-    command_msg.names = ['right_e0', 'right_e1', 'right_s0', 'right_s1',
-                         'right_w0', 'right_w1', 'right_w2']
-    command_msg.mode=JointCommand.POSITION_MODE
-    control_rate = rospy.Rate(freq) # sending commands at freq HZ
-
-    joint_states = rospy.wait_for_message("/robot/joint_states", JointState)
-    cur_q = joint_states.position[9:16]
-    # cur_q = [cur_q[2], cur_q[3], cur_q[0], cur_q[1],
-    #          cur_q[4], cur_q[5], cur_q[6]]
+def getT(rkin):
     forward = rkin.forward_position_kinematics()
     quatangles = forward[3:]
     rot = util.quat2rot(quatangles[0], quatangles[1], quatangles[2],
                         quatangles[3])
     pos = numpy.matrix(forward[:3]).T
-    curT = util.transformation(rot, pos)
-    ori = util.dcm2angle(rot)
-    curVec = numpy.vstack([pos, ori])
-    cur_d = getD(curVec, final, scale)
-    print('cur_d:')
-    print(cur_d)
-    while (not rospy.is_shutdown() and cur_d > tolerance):
-        J = rkin.jacobian()
-        joint_states = rospy.wait_for_message("/robot/joint_states", JointState)
-        print('input names:')
-        print(joint_states.name[9:16])
-        cur_q = joint_states.position[9:16]
-        forward = rkin.forward_position_kinematics()
-        quatangles = forward[3:]
-        rot = util.quat2rot(quatangles[0], quatangles[1], quatangles[2],
-                            quatangles[3])
-        pos = numpy.matrix(forward[:3]).T
-        print('pos:')
-        print(pos)
-        print('angles:')
-        print(util.dcm2angle(rot))
-        curT = util.transformation(rot, pos)
-	### no joint limit correction
-	# q_dot = util.resolvedRates(J, curT, final, speed)
-        # out_q = list(numpy.array(
-        #    cur_q + (1./freq) * q_dot.T)[0])
-	### paper algorithm
-	q_dot = util.resolvedRates(J, curT, final, speed)
-	corrected_q_dot = q_dot + util.jointLimitPaper(
-            cur_q, qMin, qMax, angular_tol, J, 10)
-        out_q = list(numpy.array(
-           cur_q + (1./freq) * corrected_q_dot.T)[0])
-        ### hand-rolled algorithm
-        # q_dot, out_q = util.resolvedRatesWithLimits(
-        #   J, curT, final, speed, cur_q, freq, qMax, qMin, angular_tol)
-        command_msg.command = out_q
-        cur_d = getD(curVec, final, scale)
-        print('cur_d:')
-        print(cur_d)
-        print('q_dot:')
-        print(q_dot)
-        print('cur_q:')
-        print(cur_q)
-        print('next q:')
-        print(command_msg.command)
-        print('command_msg:')
-        print(command_msg)
-        pub_joint_cmd.publish(command_msg)
-        control_rate.sleep()
-        ori = util.dcm2angle(rot)
-        curVec = numpy.vstack([pos, ori])
+    return util.transformation(rot, pos) 
 
-# out of order
-joint_maxes = [3.028, 2.618, .89, 1.047, 3.059, 
-               2.094, 3.059]
-joint_mins = [-3.028, -.052, -2.461, -2.147, -3.059, 
-              -1.571, -3.059]
+# get position/orientation vector (6x1)
+def getPosOriVector(T):
+    rot = T[:3,:3]
+    pos = T[:3, 3]
+    ori = util.dcm2angle(rot)
+    return numpy.vstack([pos, ori])
+
+# ripped from baxter website
+joint_maxes = [3.028, 2.618, .89, 1.047, 3.059, 2.094, 3.059]
+joint_mins = [-3.028, -.052, -2.461, -2.147, -3.059, -1.571, -3.059]
 middles = map(lambda (a, b): (a + b) / 2, zip(joint_maxes, joint_mins))
-# joint_maxes = [jmax[2], jmax[3], jmax[0], jmax[1],
-#                jmax[4], jmax[5], jmax[6]]
-# joint_mins = [jmin[2], jmin[3], jmin[0], jmin[1],
-#               jmin[4], jmin[5], jmin[6]]
 
 def main():
     print("Initializing node... ")
     rospy.init_node("examples")   # the node's name is examples
     msg = rospy.wait_for_message("/robot/joint_states", JointState)
-    print(msg)
-    print(msg.name[9:16])
-    print(msg.position[9:16])
-    # return
-    print('initializing kinematics...')
     rkin = baxter_kinematics('right')
-    print('forward kinematics:')
-    forward = rkin.forward_position_kinematics()
-    print(forward)
-    quatangles = forward[3:]
-    print('angles:')
-    rot = util.quat2rot(quatangles[0], quatangles[1], quatangles[2],
-                        quatangles[3])
-    print(rot)
-    print('dcm2angle:')
-    print(util.dcm2angle(rot))
-    print('position:')
-    pos = numpy.matrix(forward[:3]).T
-    print(pos)
-    print('transformation:')
-    trans = util.transformation(rot, pos)
-    print(trans)
-    print('jocabian:')
-    J = rkin.jacobian()
-    print(J)
     ### top left
-    send_to_joint_vals([-numpy.pi/6,numpy.pi/5,-numpy.pi/3,-numpy.pi/3,0,numpy.pi/2,0])
+    # send_to_joint_vals([-numpy.pi/6,numpy.pi/5,-numpy.pi/3,-numpy.pi/3,0,numpy.pi/2,0])
     ### side
     # send_to_joint_vals(middles)
     ### front
-    # send_to_joint_vals([-0.117349530139,0,
-    #                    1.01319430924, 0,
-    #                    0.153398078613,0.626247655939,3.05108778362])
+    send_to_joint_vals([-0.117349530139,0, 1.01319430924, 0, 0.153398078613,0.626247655939,3.05108778362])
     ### desired
-    # send_to_joint_vals([-0.117349530139,1.65017983068,
-    #                     1.01319430924,-0.73784475813,
-    #                     0.153398078613,0.626247655939,3.05108778362])
-    forward = rkin.forward_position_kinematics()
-    quatangles = forward[3:]
-    dcm = util.dcm2angle(util.quat2rot(quatangles[0], quatangles[1],
-                                       quatangles[2], quatangles[3]))
-    pos = numpy.matrix(forward[:3]).T
-    print('position:')
-    print(pos)
-    print('rotation:')
-    print(dcm)
+    # send_to_joint_vals([-0.117349530139,1.65017983068, 1.01319430924,-0.73784475813, 0.153398078613,0.626247655939,3.05108778362])
     final = numpy.matrix([[.67206115, -.16257794, .01516517,
                            3.12855761, -.03523214, .12796079]]).T
-    speed = [2, .1]
-    scale = [1, .2]
-    runResolvedRates(rkin, final, speed, 100, scale, .1,
-                     joint_maxes, joint_mins, 0.1)
+
+    pub_joint_cmd = rospy.Publisher(
+        '/robot/limb/right/joint_command',JointCommand)
+    command_msg = JointCommand()
+    command_msg.names = ['right_e0', 'right_e1', 'right_s0', 'right_s1',
+                         'right_w0', 'right_w1', 'right_w2']
+    command_msg.mode=JointCommand.POSITION_MODE
+
+    speed = 2
+    alpha = .5
+    freq = 100
+    tolerance = .01
+    runResolvedRates(
+        rkin, 
+        lambda q: publish_joints(pub_joint_cmd, command_msg, q),
+        final, speed, freq, tolerance, alpha, joint_maxes, joint_mins)
+    
+# assumes that pub_joint_cmd and cmd_msg are appropriately constructed
+def publish_joints(cmd, cmd_msg, q):
+    cmd_msg.command = [float(q_i) for q_i in q]
+    cmd.publish(cmd_msg)
 
 if __name__ == '__main__':
     main()
--- util.py
+++ util.py
@@ -1,28 +1,14 @@
 import numpy
-import numpy.linalg
-
-def makeTransformMat(rot, pt):
-    ret = numpy.zeros((4, 4))
-    for i in range(0, 3):
-        for j in range(0, 3):
-            ret[i,j] = rot[i,j]
-        ret[i,3] = pt[i]
-        ret[3,i] = 0
-    ret[3,3] = 1
-    return ret
-
-def unpackTransformMat(m):
-    return (numpy.matrix(m[0:3,0:3]), numpy.matrix(m[0:3, 3]))
 
 def hat3(v):
     return numpy.matrix([[0,-v[2],v[1]],
                          [v[2],0,-v[0]],
                          [-v[1],v[0],0]])
 def hat6(v):
-    return numpy.matrix([0,-v[5],v[4],v[0]],
-                        [v[5],0,-v[3],v[1]],
-                        [-v[4],v[3],0,v[2]],
-                        [0,0,0,0])
+    return numpy.matrix([[0,-v[5],v[4],v[0]],
+                         [v[5],0,-v[3],v[1]],
+                         [-v[4],v[3],0,v[2]],
+                         [0,0,0,0]])
 def wedge3(vhat):
     return numpy.array([vhat[2,1],vhat[0,2],vhat[1,0]])
 
@@ -31,20 +17,51 @@ def wedge6(vhat):
         [vhat[0,3], vhat[1,3], vhat[2,3],
          vhat[2,1], vhat[0,2], vhat[1,0]])
 
-# n in the below comments is the number of joints
-# J is the hubrid jocabian matrix (6 x n)
-# T is a transformation from the base frame representing current pos/ori (6 x n)
-# aka transform from base to end effector
-# target is a transformation from the base frame (pos/ori) (6 x n)
-def resolvedRates(J, T, target, speed):
-    linDistance = numpy.matrix(target[0:3,0]-T[0:3,3])
-    angDistance = numpy.matrix(target[3:,0]-dcm2angle(T[0:3,0:3]))
-    linVelocity = speed[0]*(linDistance/numpy.linalg.norm(linDistance))
-    angVelocity = speed[1]*(angDistance/numpy.linalg.norm(angDistance))
-    velocity = numpy.r_[linVelocity,angVelocity] # 6 x 1 in the hubrid frem!!
-    # velocity = numpy.r_[linVelocity, numpy.matrix([[0],[0],[0]])]
-    qdot = numpy.linalg.pinv(J)*velocity
-    return qdot
+# 6x1 [x,y,z,theta_x,theta_y,theta_z]
+def posOriToTransformMat(V):
+    R = angle2dcm(V[3:,0])
+    p = V[:3,0]
+    return transformation(R, p)
+
+# alpha is scalar
+# tol is scalar
+# getJacobian is function which accepts no arguments
+# waiter is an object with a sleep function which accepts no arguments
+# angles_get fun, no args
+# getT: fun, no args
+# posori_get: fun, takes transformation matrix, returns 6x1 pos/ori vector
+# publish_fun: recieve desired q
+# x_des: target position
+# dt: delta time
+def resolvedRates(alpha, tol, getJacobian, waiter, angles_get, getT, posori_get, publish_fun, x_des, speed, dt):
+    # linear trajectory, so no explicit trajectory array
+    # drawn directly from solutions for assignment 5
+    t = 0
+    T = getT()
+    x_cur = posori_get(T)
+    x0 = x_cur
+    x_err = x_des - x_cur
+    v_des = speed * x_err / numpy.linalg.norm(x_err)
+
+    # v_matrix_des = speed * 
+    # waiter should be NEWLY CONSTRUCTED
+    waiter.sleep()              # start off sleeping
+    
+    while numpy.linalg.norm(x_err) > tol:
+        g_inv = numpy.linalg.inv(T)
+        g_d = posOriToTransformMat(x0 + v_des * t)
+        p_cur_err = logtr(g_inv * g_d)
+        J = getJacobian()
+        q_dot = numpy.linalg.pinv(J) * (v_des + alpha * p_cur_err)
+        # TODO: call correction function here to modify q_dot, if applicable
+        q = angles_get() + q_dot * dt
+        t += dt
+        publish_fun(q)
+
+        waiter.sleep()
+        T = getT()
+        x_cur = posori_get(T)
+        x_err = x_des - x_cur
 
 def quat2rot(qx,qy,qz,qw):
     return numpy.matrix(
@@ -56,6 +73,46 @@ def transformation(R,p):
     T = numpy.c_[R, p]
     return numpy.r_[T,[[0, 0, 0, 1]]]
 
+def angle2dcm(inVect, input_unit='rad',
+              rotation_sequence='ZYX', output_type='matrix'):
+    inVect *= -1
+    rotAngle1 = inVect[0,0]
+    rotAngle2 = inVect[1,0]
+    rotAngle3 = inVect[2,0]
+    # switch ZYX->XYZ
+    tmp = rotAngle1
+    rotAngle1 = rotAngle3
+    rotAngle3 = tmp
+
+    R3 = numpy.zeros((3, 3))
+    R2 = numpy.zeros((3, 3))
+    R1 = numpy.zeros((3, 3))
+    if(input_unit == 'deg'):
+        rotAngle1 = numpy.deg2rad(rotAngle1)
+        rotAngle2 = numpy.deg2rad(rotAngle2)
+        rotAngle3 = numpy.deg2rad(rotAngle3)
+    R3[2, 2] = 1.0
+    R3[0, 0] = numpy.cos(rotAngle1)
+    R3[0, 1] = numpy.sin(rotAngle1)
+    R3[1, 0] = -numpy.sin(rotAngle1)
+    R3[1, 1] = numpy.cos(rotAngle1)
+    R2[1, 1] = 1.0
+    R2[0, 0] = numpy.cos(rotAngle2)
+    R2[0, 2] = -numpy.sin(rotAngle2)
+    R2[2, 0] = numpy.sin(rotAngle2)
+    R2[2, 2] = numpy.cos(rotAngle2)
+    R1[0, 0] = 1.0
+    R1[1, 1] = numpy.cos(rotAngle3)
+    R1[1, 2] = numpy.sin(rotAngle3)
+    R1[2, 1] = -numpy.sin(rotAngle3)
+    R1[2, 2] = numpy.cos(rotAngle3)
+    if rotation_sequence == 'ZYX':
+        C = R1.dot(R2.dot(R3))
+    else:
+        raise NotImplementedError('Rotation sequences other than ZYX are not currently implemented')
+    if(output_type == 'matrix'): C = numpy.matrix(C)
+    return C
+
 def dcm2angle(C, output_unit='rad', rotation_sequence='ZYX'):
     # From navpy outputs x,y,z angles
     if(C.shape[0] != C.shape[1]):
@@ -63,20 +120,21 @@ def dcm2angle(C, output_unit='rad', rotation_sequence='ZYX'):
     if(C.shape[0] != 3):
         raise ValueError('Matrix is not 3x3')
 
-    if(rotation_sequence == 'ZYX'):
-        rotAngle1 = numpy.arctan2(C[0, 1], C[0, 0])   # Yaw
-        rotAngle2 = -numpy.arcsin(C[0, 2])  # Pitch
-        rotAngle3 = numpy.arctan2(C[1, 2], C[2, 2])  # Roll
-    else:
-        raise NotImplementedError('Rotation sequences other than ' +
-		' ZYX are not currently implemented')
+    rotAngle1 = numpy.arctan2(C[0, 1], C[0, 0])   # Yaw
+    rotAngle2 = -numpy.arcsin(C[0, 2])  # Pitch
+    rotAngle3 = numpy.arctan2(C[1, 2], C[2, 2])  # Roll
 
     if(output_unit == 'deg'):
         rotAngle1 = numpy.rad2deg(rotAngle1)
         rotAngle2 = numpy.rad2deg(rotAngle2)
         rotAngle3 = numpy.rad2deg(rotAngle3)
 
-    return numpy.matrix([[rotAngle3], [rotAngle2], [rotAngle1]])
+    return -1 * numpy.matrix([[rotAngle3], [rotAngle2], [rotAngle1]])
+
+def transformMatToPosOri(T):
+    pos = T[:3,3]
+    ori = dcm2angle(T[:3,:3])
+    return numpy.vstack([pos, ori])
 
 def zeroOutColOfMatrix(mat, col):
     retmat = numpy.copy(mat)
@@ -123,3 +181,24 @@ def jointLimitPaper(theta, thetamin, thetamax,alpha,J,k):
     pinv_times_J = numpy.linalg.pinv(J)*J
     dp_dtheta_vect = numpy.matrix([dp_dtheta]).T
     return (numpy.identity(pinv_times_J.shape[0])-pinv_times_J)*(-k*dp_dtheta_vect)
+
+def logtr(g):
+    R = g[0:3,0:3]
+    theta = numpy.arccos((numpy.trace(R)-1)/2)
+    p = g[0:3,3]
+
+    if numpy.absolute(theta) < 1e-8:
+        w = numpy.transpose(numpy.matrix([0, 0, 0]))
+        v = p
+    else:
+        w = numpy.zeros((3,1))
+        w[0] = 1/2/numpy.sin(theta)*(R[2,1]-R[1,2])
+        w[1] = 1/2/numpy.sin(theta)*(R[0,2]-R[2,0])
+        w[2] = 1/2/numpy.sin(theta)*(R[1,0]-R[0,1])
+
+        A = (numpy.identity(3)-R)*hat3(w)+w*w.T*theta
+        v = numpy.linalg.solve(A,p)
+        w = w*theta
+        v = v*theta
+
+    return hat6(numpy.vstack([v,w]))
