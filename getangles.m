70c958cb6a4b3e897356c7af587767e8370b2ce3
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-12-08 01:13:27 -0600

    changes

--- getangles.m
+++ getangles.m
@@ -1,15 +1,15 @@
 %function [output1,output2] = getangles(args)
-function main
+function getangles
     %theta = args.arg1';
     %final = args.arg2;
     type = 6;
     theta = [1;-0.7;0;1.4;0;0.85;0];
     %theta = [1;-0.377;-0.2;1.4;0.1;0.777;0.067];
-    final = [0.5,.5,-.1];
+    final = [0.3,.5625,.01516];
     finalr = final + [0,0,1.0374-0.8077];
     finalr = finalr';
     Rdes = ROTy(pi/2);
-    
+
     dt = 0.1;
     w = [0,0,1,0,1,0,1;0,1,0,1,0,1,0;1,0,0,0,0,0,0];
     q = [0,0.069,0.171,0.4334,0.537,0.8077,0.9237;0,0,0,0,0,0,0;...
@@ -18,7 +18,7 @@ function main
     for i = 1:7
         tw(:,i) = [-cross(w(:,i),q(:,i));w(:,i)];
     end
-    
+
     g01 = eye(4);
     g02 = [ROTx(-pi/2),[0.0690;0;0.2703];zeros(1,3),1]
     g03 = [ROTy(pi/2),[0.1710;0;0.2703];zeros(1,3),1];
@@ -30,10 +30,10 @@ function main
     %g0e = [ROTy(pi/2),[1.0237;0;0.1913];zeros(1,3),1];
 
 
-    
+
     framecount = 1;
     q = 1;
-    
+
     while true
         q=q+1;
         if floor(q/200) == ceil(q/200)
@@ -50,7 +50,7 @@ function main
         H{6} = ex{1}*ex{2}*ex{3}*ex{4}*ex{5}*ex{6}*g06;
         H{7} = ex{1}*ex{2}*ex{3}*ex{4}*ex{5}*ex{6}*ex{7}*g07;
         He = ex{1}*ex{2}*ex{3}*ex{4}*ex{5}*ex{6}*ex{7}*g0e;
-        
+
         Oe = He(1:3,4);
         O = zeros(3,7);
         J = zeros(6,7);
@@ -65,7 +65,7 @@ function main
         DrawRobot(O,Oe,final,finalr);
         movie_frames(framecount) = getframe(gcf);
         framecount = framecount+1;
-        
+
         CurrR = He(1:3,1:3);
         %Reul = rotm2eul(R,'XYZ');
         %ErrR = dot(Rdes,CurrR')
@@ -77,8 +77,8 @@ function main
         w_unit = me;
         EdotR = .001*w_unit;
         %Rm = R\Rdes;
-        
-        %[r,p,y] = dcm2angle(Rm, 'XYZ')        
+
+        %[r,p,y] = dcm2angle(Rm, 'XYZ')
         %V = (1/cos(p))*[cos(r) sin(r) 0;-sin(r)*cos(p) cos(r)*cos(p) 0;cos(r)*sin(p) sin(r)*sin(p) cos(r)];
         %E = [eye(3,3) zeros(3,3); zeros(3,3) V];
         %J = E*JE;
@@ -132,11 +132,11 @@ function main
                 pause(5);
                 break
             end
-            qdot = J'*(J*J'+0.01*eye(6,6))^(-1)*Edot; 
+            qdot = J'*(J*J'+0.01*eye(6,6))^(-1)*Edot;
         end
-        theta = theta + qdot*dt;
+        theta = theta + qdot*dt
     end
-    
+
      nFrames = length(movie_frames);
      vidObj = VideoWriter('Baxter_Checkers.avi');
      vidObj.Quality = 100;
@@ -147,7 +147,7 @@ function main
      end
      close(gcf)
      close(vidObj);
-    
+
     output1 = theta;
     output2 = He;
 end
@@ -159,10 +159,10 @@ function DrawRobot(O,Oe,final,finalr)
     axis([0,1.5,0,1.5,-1,1]); % Defines axes bounds
     view([1,1,1]); %View angle
     hold on;
-    
+
     % Plots point and lines for links
-    
-    
+
+
     for i=1:7
         plot3(O(1,i),O(2,i),O(3,i),'ro');
         if i == 7
@@ -175,7 +175,7 @@ function DrawRobot(O,Oe,final,finalr)
             line([O(1,i),O(1,i+1)],[O(2,i),O(2,i+1)],[O(3,i),O(3,i+1)]);
         end
     end
-    
+
 end
 
 function [R] = ROTx(theta)
